df12596fe6dffb556bea75d8515dfe1151bd4864
kakuni1 <127331940+kakuni1@users.noreply.github.com>
2026-04-27 17:32:35 -0700

    import background image

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import "./styles.css";
+import background from "./assets/images/background.webp";
+
+const imageBackground = document.createElement("img");
+const container = document.createElement("div");
+
+imageBackground.src = background;
+imageBackground.alt = "a room filled with lots of different types of records";
+imageBackground.id = "background-image";
+container.id = "container-image";
+
+container.appendChild(imageBackground);
+document.body.appendChild(container);
