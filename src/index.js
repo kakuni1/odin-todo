e059c4ac7f2dcd10f2b5e0f5961f2e4388077041
kakuni1 <127331940+kakuni1@users.noreply.github.com>
2026-06-10 11:43:28 -0700

    reorder

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import { loadInitialSidebar } from "./modules/sidebar-initial.js";
 const addButton = document.createElement("button");
 const sidebar = document.createElement("div");
 
-addButton.classList.add("add-button");
 sidebar.id = "container-sidebar";
 
+addButton.classList.add("add-button");
 addButton.innerHTML = icons["icon-add"];
-loadInitialSidebar(sidebar);
 
+loadInitialSidebar(sidebar);
 sidebar.appendChild(addButton);
 document.body.appendChild(sidebar);
