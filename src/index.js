434d8f15ee3b2801e90703adf1bd88deb0a75606
kakuni1 <127331940+kakuni1@users.noreply.github.com>
2026-06-10 11:29:42 -0700

    remove: background image

--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
-import { importBackgroundImage } from "./modules/background-image.js";
 import { icons } from "./modules/icons.js";
 import { loadInitialSidebar } from "./modules/sidebar-initial.js";
 
-const background = document.createElement("div");
 const addButton = document.createElement("button");
 const sidebar = document.createElement("div");
 
 addButton.classList.add("add-button");
-background.id = "container-image";
 sidebar.id = "container-sidebar";
 
 addButton.innerHTML = icons["icon-add"];
-importBackgroundImage(background);
 loadInitialSidebar(sidebar);
 
 sidebar.appendChild(addButton);
 document.body.appendChild(sidebar);
-document.body.appendChild(background);
