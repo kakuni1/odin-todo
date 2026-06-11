d7e8e900e247264ff9220c95457f3440837510c2
kakuni1 <127331940+kakuni1@users.noreply.github.com>
2026-06-10 22:29:14 -0700

    add: sidebar icons (expand & collapse)

--- src/modules/icons.js
+++ src/modules/icons.js
@@ -10,7 +10,7 @@ export const icons = {
       stroke-width="2"
       stroke-linecap="round"
       stroke-linejoin="round"
-      class="tabler-icon-plus"
+      class="icon-plus"
     >
       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
       <path
@@ -34,7 +34,7 @@ export const icons = {
       stroke-width="2"
       stroke-linecap="round"
       stroke-linejoin="round"
-      class="tabler-icon-minus"
+      class="icon-minus"
     >
       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
       <path
@@ -44,6 +44,48 @@ export const icons = {
         d="M4.012 16.737a2 2 0 0 1 -1.012 -1.737v-10c0 -1.1 .9 -2 2 -2h10c.75 0 1.158 .385 1.5 1"
       />
       <path d="M11 14h6" />
+    </svg>
+  `,
+  "icon-sidebar-collapse": `
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="icon-sidebar-collapse"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path
+        d="M4 6a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2l0 -12"
+      />
+      <path d="M9 4v16" />
+      <path d="M15 10l-2 2l2 2" />
+    </svg>
+  `,
+  "icon-sidebar-expand": `
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="icon-sidebar-expand"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path
+        d="M4 6a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2l0 -12"
+      />
+      <path d="M9 4v16" />
+      <path d="M14 10l2 2l-2 2" />
     </svg>
   `,
 };
