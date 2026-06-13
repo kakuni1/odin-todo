5ecdda5a75f3b281b1d32a98ab0988b9d69e5d0d
kakuni1 <127331940+kakuni1@users.noreply.github.com>
2026-06-13 15:28:40 -0700

    rename: button

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import { icons } from "./modules/icons.js";
 import { sidebarLoad } from "./modules/sidebar.js";
 
-const buttonSidebar = document.createElement("button");
+const sidebarButton = document.createElement("button");
 const mainArea = document.createElement("div");
 const sidebar = document.createElement("div");
 
 sidebar.id = "container-sidebar";
 mainArea.id = "container-main";
 
-buttonSidebar.classList.add("button-sidebar-collapse");
-buttonSidebar.innerHTML = icons["icon-sidebar-collapse"];
+sidebarButton.classList.add("button-sidebar-collapse");
+sidebarButton.innerHTML = icons["icon-sidebar-collapse"];
 
 sidebarLoad(sidebar);
-sidebar.appendChild(buttonSidebar);
+sidebar.appendChild(sidebarButton);
 document.body.appendChild(sidebar);
 document.body.appendChild(mainArea);
