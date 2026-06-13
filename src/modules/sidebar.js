5b4864a7aec90360e59973a7482c642c8f32e583
kakuni1 <127331940+kakuni1@users.noreply.github.com>
2026-06-13 15:24:50 -0700

    fix: rename

--- src/modules/sidebar.js
+++ src/modules/sidebar.js
@@ -1,4 +1,4 @@
-export function sidebarInitialState(container) {
+export function sidebarLoad(container) {
 	const h2 = document.createElement("h2");
 	h2.textContent = "Projects";
 	container.appendChild(h2);
