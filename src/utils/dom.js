1a22713b450a6e07cb2323ff7d649ad2b56b8b74
kakuni1 <127331940+kakuni1@users.noreply.github.com>
2026-06-18 11:22:05 -0700

    add: entry highlight & end icon

--- src/utils/dom.js
+++ src/utils/dom.js
@@ -1,15 +1,17 @@
 import { getIcon } from "./icons.js";
 
 /**
- * @param {string} iconName
+ * @param {string} iconFront
+ * @param {string} iconEnd
  * @param {string} label
  */
-export function createListItem(iconName, label) {
+export function createListItem(iconFront, iconEnd, label) {
 	const li = document.createElement("li");
-	li.innerHTML = getIcon(iconName);
+	li.innerHTML = getIcon(iconFront);
 	const span = document.createElement("span");
 	span.className = "list-item";
 	span.textContent = label;
 	li.appendChild(span);
+	if (iconEnd) li.insertAdjacentHTML("beforeend", getIcon(iconEnd));
 	return li;
 }
