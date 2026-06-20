59c46d6a5385d5aa0d5e054dda38ede7505577b4
kakuni1 <127331940+kakuni1@users.noreply.github.com>
2026-06-20 16:39:13 -0700

    add: modal event listeners

--- src/modules/events.js
+++ src/modules/events.js
@@ -1,3 +1,4 @@
+import { closeModal, createModal, openModal } from "./modals.js";
 import { sideButtonSwap } from "./sidebar.js";
 
 /**
@@ -8,4 +9,18 @@ export function initSidebar(sideButton, sideArea) {
   sideButton.addEventListener("click", () => {
     sideButtonSwap(sideArea, sideButton);
   });
+  const button = document.getElementById("remove-folder");
+  if (button) {
+    button.addEventListener("click", () => {
+      const projectName = document.getElementById("remove-folder").dataset.project;
+      const overlay = createModal(`Remove Project: ${projectName}`);
+      document.body.appendChild(overlay);
+      openModal(overlay);
+      const close = overlay.getElementsByClassName("modal-close-button");
+      close[0].addEventListener("click", () => {
+        closeModal(overlay);
+        overlay.remove();
+      });
+    });
+  }
 }
