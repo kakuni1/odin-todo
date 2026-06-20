7195b540a1635150224128ae814ac02d4e8f4607
kakuni1 <127331940+kakuni1@users.noreply.github.com>
2026-06-20 16:59:03 -0700

    add: comments (event listeners)

--- src/modules/events.js
+++ src/modules/events.js
@@ -6,16 +6,19 @@ import { sideButtonSwap } from "./sidebar.js";
  * @param {HTMLElement} sideArea
  */
 export function initSidebar(sideButton, sideArea) {
+  // click on sidebar button -> hide/show sidebar
   sideButton.addEventListener("click", () => {
     sideButtonSwap(sideArea, sideButton);
   });
   const button = document.getElementById("remove-folder");
   if (button) {
+    // click on close button (delete folder) -> confirmation modal
     button.addEventListener("click", () => {
       const projectName = document.getElementById("remove-folder").dataset.project;
       const overlay = createModal(`Remove Project: ${projectName}`);
       document.body.appendChild(overlay);
       openModal(overlay);
+      // click on close modal icon -> close modal
       const close = overlay.getElementsByClassName("modal-close-button");
       close[0].addEventListener("click", () => {
         closeModal(overlay);
