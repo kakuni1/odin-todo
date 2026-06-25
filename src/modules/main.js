8b37774f36914ed07f75b7af686b2eae868f25ed
kakuni1 <127331940+kakuni1@users.noreply.github.com>
2026-06-24 17:15:57 -0700

    add: folder overview (main)

--- src/modules/main.js
+++ src/modules/main.js
@@ -3,3 +3,10 @@ export function createMain() {
   main.id = "main";
   return main;
 }
+
+export function sectionClick() {
+  const sectionOverview = document.createElement("div");
+  sectionOverview.className = "folder-overview";
+  document.querySelector("#main").replaceChildren();
+  document.querySelector("#main").appendChild(sectionOverview);
+}
