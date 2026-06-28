1d22ceb13d3a90deb88eedb22e12f055c3ee769e
kakuni1 <127331940+kakuni1@users.noreply.github.com>
2026-06-27 17:07:52 -0700

    change: date format

--- src/modules/date-print.js
+++ src/modules/date-print.js
@@ -1,6 +1,6 @@
 import { format } from "date-fns";
 
 export function printDate() {
-  const today = format(new Date(), "MM/dd/yyyy");
+  const today = format(new Date(), "yyyy/MM/dd");
   return today;
 }
