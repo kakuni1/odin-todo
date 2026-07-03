b027eec99d85fc1c812143dc2d164cae08a9ac0f
kakuni1 <127331940+kakuni1@users.noreply.github.com>
2026-07-02 17:57:57 -0700

    add: date (functions)

--- src/modules/date.js
+++ src/modules/date.js
@@ -10,3 +10,31 @@ export function formatDate(date) {
   const combinedFullDate = `${year}/${month}/${day}`;
   return combinedFullDate;
 }
+
+export function thisWeek(date) {
+  const inputDate = new Date(`${date}T00:00:00`);
+  const nowDate = new Date();
+
+  // reset to sunday
+  inputDate.setDate(inputDate.getDate() - inputDate.getDay());
+  nowDate.setDate(nowDate.getDate() - nowDate.getDay());
+
+  // reset time
+  inputDate.setHours(0, 0, 0, 0);
+  nowDate.setHours(0, 0, 0, 0);
+
+  // check same week (same sunday)
+  return inputDate.getTime() === nowDate.getTime();
+}
+
+export function thisDay(date) {
+  const inputDay = new Date(`${date}T00:00:00`);
+  const nowDay = new Date();
+
+  // reset time
+  inputDay.setHours(0, 0, 0, 0);
+  nowDay.setHours(0, 0, 0, 0);
+
+  // check same day (time reset)
+  return inputDay.getTime() === nowDay.getTime();
+}
